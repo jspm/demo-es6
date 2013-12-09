cd10ba1f08730734aecb857a62f2048f584bc414
guybedford <guybedford@gmail.com>
2013-12-09 12:21:47 +0200

    latest test

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 jspm.config({
-  "baseURL": "app",
+  "baseURL": "app-build",
   "map": {
     "jquery": "github:components/jquery@2.0.3"
   },
-  "jspmPackages": "jspm_packages",
+  "jspmPackages": false,
   "packages": {
     "github:components/jquery@2.0.3": {
       "main": "jquery"
